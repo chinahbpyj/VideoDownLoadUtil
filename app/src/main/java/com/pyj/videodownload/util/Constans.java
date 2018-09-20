00f0ad286b2450c25aac1a2f2b2007cb3bd7ec40
chinahbpyj <chinahbpyj@163.com>
2018-09-20 17:34:51 +0800

    update

---
+++ app/src/main/java/com/pyj/videodownload/util/Constans.java
@@ -0,0 +1,12 @@
+package com.pyj.videodownload.util;
+
+public class Constans {
+    public static final String source_zzs="种子搜";
+    public static final String source_clb="磁力吧";
+    public static final String source_bttz="BT兔子";
+    public static final String source_btdb="BTDB";
+    public static final String source_bt4g="BT4G";
+    public static final String source_dss="屌丝搜";
+
+    public static final String keyword="胡歌";
+}
